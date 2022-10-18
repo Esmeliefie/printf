1c0d5d1a4bf2d81d0318c2ddd3d3b6fa25d1a3f0
Esmeralda Kok <esykok@gmail.com>
2022-10-18 02:48:48 -0700

    prinf handling func Esmeralda

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
  * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
  * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
+ * determine which printing function to call
  * specifiers contained into fmt
  * Return: length of the formatted output string
  */
@@ -13,12 +13,9 @@ int _printf(const char *format, ...)
 	int (*pfunc)(va_list, flags_t *);
 	const char *p;
 	va_list arguments;
-
 	flags_t flags = {0, 0, 0};
 
 	register int count = 0;
-
-
 
 	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
